e1e203024485a238eb16e03930d312d9e0527bc4
tnn225 <tnn225@gmail.com>
2012-06-18 23:04:38 -0700

    Allow RidgeRegression tune monthly

--- learn/RidgeRegression.py
+++ learn/RidgeRegression.py
@@ -5,15 +5,13 @@ import Score
 
 class RidgeRegression(object):
   # k_values = [8, 9, 10, 11, 12.5, 14]
-  k_values = [10]
+  k_values = [6, 7]
   def __init__(self, k=10, debug=False):
-    self.A = None
-    self.A_T = None
     self.debug = debug
-    self.k = k
+    self.k_list = None
     self.params = None
     
-  def _train_with_k(self, dataset, k):
+  def _train(self, dataset, k, k_list = None):
     # http://en.wikipedia.org/wiki/Tikhonov_regularization
     # <math>\hat{x} = (A^{T}A+ \Gamma^{T} \Gamma )^{-1}A^{T}\mathbf{b}</math>
 
@@ -31,6 +29,9 @@ class RidgeRegression(object):
       
       A = np.hstack((month_features, np.ones((num_samples,1))))
       A_T = A.transpose()
+
+      if k_list:
+        k = k_list[month_ind]
       
       month_params = np.linalg.inv(A_T.dot(A) + k**2 * np.identity(num_features+1)).dot(A_T).dot(dataset.getSalesForMonth(month_ind))
       params[:, month_ind] = month_params
@@ -39,30 +40,34 @@ class RidgeRegression(object):
     
   def cross_validate(self, dataset, num_folds):
     dataset.createFolds(num_folds)
-    best_k = None
-    best_rmsle = float("inf")
+
+    best_rmsle_list = [float("inf")] * 12
+    self.k_list = [0] * 12
+
     for k in RidgeRegression.k_values:
       if self.debug:  
         print "Running ridge regression with k=%f on %d folds" %(k, num_folds)
 
-      k_score = Score.Score()
+      score = Score.Score()
+
       for fold_ind in range(num_folds):
         fold_train = dataset.getTrainFold(fold_ind)
         fold_test = dataset.getTestFold(fold_ind)
-        self._train_with_k(fold_train, k)
-        k_score.addFold(fold_test.getSales(), self.predict(fold_test))
-        
-      cur_rmsle = k_score.getRMSLE()
-      if cur_rmsle < best_rmsle:
-        if self.debug:
-          print "Achieved new best rmsle %f" %cur_rmsle
-        best_k = k
-        best_rmsle = cur_rmsle
-        
-    self.k = best_k
+        self._train(fold_train, k)
+        score.addFold(fold_test.getSales(), self.predict(fold_test))
+
+      for month_ind in range(12):
+        cur_rmsle = score.getRMSLE(month_ind)
+
+        if cur_rmsle < best_rmsle_list[month_ind]:
+          best_rmsle_list[month_ind] = cur_rmsle
+          self.k_list[month_ind] = k
         
   def train(self, dataset):
-    self._train_with_k(dataset, self.k)
+    if self.debug:
+      print "Training ridge regression with k_list: %s" %(self.k_list)
+
+    self._train(dataset, None, self.k_list)
     
   def predict(self, dataset):
     features = dataset.getFeatures()
@@ -74,14 +79,3 @@ class RidgeRegression(object):
 
     
 LearnerBase.register(RidgeRegression)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
