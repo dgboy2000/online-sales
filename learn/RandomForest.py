76a8cadd241e1245185f90b1a5a4f77c18cec8f8
tnn225 <tnn225@gmail.com>
2012-06-18 22:38:47 -0700

    Add ability to score monthly in Score.py, allow RidgeRegression tune for each month separately

--- learn/RandomForest.py
+++ learn/RandomForest.py
@@ -5,60 +5,70 @@ from scipy import linalg
 import Score
 
 class RandomForest(object):
-  # n_values = [60, 80, 100, 125, 150]
-  # split_values = [1, 2, 3, 4]
-  n_values = [10]
-  split_values = [2]
+  n_values = [60, 80, 100, 125, 150]
+  split_values = [1, 2, 3, 4]
   
   def __init__(self, n_estimators=100, min_split=2, debug=False):
-    self.rf_list = None
     self.debug = debug
-    self.min_split = min_split
-    self.n_estimators = n_estimators
-    self.params = None
 
+    self.rf_list = None
+    self.n_estimators_list = None
+    self.min_samples_split_list = None
 
-  def _train_with_values(self, dataset, n_estimators, min_samples_split):
+  def _train(self, dataset, 
+             n_estimators, 
+             min_samples_split,
+             n_estimators_list = None,
+             min_samples_split_list = None):
     self.rf_list = []
+
     for month_ind in range(12):
       month_sales = dataset.getSalesForMonth(month_ind)
+
       if self.debug:
         print "Learning on month %d of 12 with %d samples..." %(month_ind+1, len(month_sales))
 
-      rf = RandomForestRegressor(n_estimators=n_estimators, min_samples_split=min_samples_split)
+      if n_estimators_list:
+        rf = RandomForestRegressor(n_estimators=n_estimators_list[month_ind], min_samples_split=min_samples_split_list[month_ind])
+      else:
+        rf = RandomForestRegressor(n_estimators=n_estimators, min_samples_split=min_samples_split)
+
       rf.fit(dataset.getFeaturesForMonth(month_ind), month_sales)
       self.rf_list.append(rf)
 
   def cross_validate(self, dataset, num_folds):
     dataset.createFolds(num_folds)
-    best_params = None
-    best_rmsle = float("inf")
+    
+    best_rmsle_list = [float("inf")] * 12
+    self.n_estimators_list = [0] * 12
+    self.min_samples_split_list = [0] * 12
+    
     for n in RandomForest.n_values:
       for split in RandomForest.split_values:
-        if self.debug:  
-          print "Running random forest with n_estimators=%d, min_split=%d on %d folds" %(n, split, num_folds)
 
-        cur_score = Score.Score()
+        score = Score.Score()
 
         for fold_ind in range(num_folds):
+          if self.debug:  
+            print "Running random forest with n_estimators=%d, min_split=%d on fold %d of %d folds" %(n, split, fold_ind, num_folds)
+
           fold_train = dataset.getTrainFold(fold_ind)
           fold_test = dataset.getTestFold(fold_ind)
-          self._train_with_values(fold_train, n, split)
-          cur_score.addFold(fold_test.getSales(), self.predict(fold_test))
-          
-        cur_rmsle = cur_score.getRMSLE()
-        if cur_rmsle < best_rmsle:
-          if self.debug:
-            print "Achieved new best score %f" %cur_rmsle
-          best_params = (n, split)
-          best_rmsle = cur_rmsle
+          self._train(fold_train, n, split)
+          score.addFold(fold_test.getSales(), self.predict(fold_test))
         
-    self.n_estimators, self.min_split = best_params
+        for month_ind in range(12):
+          cur_rmsle = score.getRMSLE(month_ind)
+
+          if cur_rmsle < best_rmsle_list[month_ind]:
+            best_rmsle_list[month_ind] = cur_rmsle
+            self.n_estimators_list[month_ind] = n
+            self.min_samples_split_list[month_ind] = split
         
   def train(self, dataset):
     if self.debug:
-      print "Training random forest with n_estimators=%d, min_split=%d" %(self.n_estimators, self.min_split)
-    self._train_with_values(dataset, self.n_estimators, self.min_split)
+      print "Training random forest with n_estimators_list: %s, min_samples_split_list: %s" %(str(self.n_estimators_list), str(self.min_samples_split_list))
+    self._train(dataset, None, None, self.n_estimators_list, self.min_samples_split_list)
     
   def predict(self, dataset):
     features = dataset.getFeatures()
@@ -71,14 +81,4 @@ class RandomForest(object):
     return predictions
     
 LearnerBase.register(RandomForest)
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
