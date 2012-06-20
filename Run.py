b596f8e1757d016e0b70f5c43a0dc42c368782fc
Danny Goodman <goodman.danny@gmail.com>
2012-06-20 16:31:58 -0700

    only linear models in ensemble

--- Run.py
+++ Run.py
@@ -60,9 +60,13 @@ class Run:
     if params.DEBUG:
       print "Training ensemble..."
     self.ensemble = learn.Ensemble.Ensemble(params.DEBUG)
+    
     self.ensemble.addLearner(learn.LinearRegression.LinearRegression(debug=params.DEBUG))
     self.ensemble.addLearner(learn.RidgeRegression.RidgeRegression(debug=params.DEBUG))
-    self.ensemble.addLearner(learn.RandomForest.RandomForest(debug=params.DEBUG))
+    # self.ensemble.addLearner(learn.RandomForest.RandomForest(debug=params.DEBUG))
+    # self.ensemble.addLearner(learn.SVMRegression.SVMRegression(debug=params.DEBUG))
+    self.ensemble.addLearner(learn.QuantLinearRegression.QuantLinearRegression(debug=params.DEBUG))
+    
     self.ensemble.train(self.ds_train, params.NUM_FOLDS)
           
   def run(self):
